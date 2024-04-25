a6ad1a33e40f60156d648dd1247da134c3c103a6
stepag01 <stepag01@easv365.dk>
2024-04-25 16:01:34 +0200

    More changes

--- Paulas_Rengoering/src/views/AboutView.vue
+++ Paulas_Rengoering/src/views/AboutView.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page....</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, illum. Quos maiores autem voluptate ipsa vero! Libero quo fuga eum omnis impedit dolore voluptatum soluta. Maiores veritatis consequatur debitis praesentium.</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
--- Paulas_Rengoering/src/views/BookingView.vue
+++ Paulas_Rengoering/src/views/BookingView.vue
@@ -2,10 +2,11 @@
     <div>
         Booking page
     </div>
+    <h1>HELLOOOO</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nihil adipisci magnam libero ratione saepe expedita atque nostrum magni vitae nemo, possimus quod ipsum tempore aperiam neque. Libero, in dolor.</p>
   </template>
   
   <script setup>
-  
   </script>
   
   <style lang="scss" scoped>
