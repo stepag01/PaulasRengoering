9a8f7a5f4f3c48745e3f2e482a77ed919eb821b6
stepag01 <stepag01@easv365.dk>
2024-04-26 14:24:24 +0200

    h1 and p insertions and  alterations + responsiveness

--- Paulas_Rengoering/src/views/GuideView.vue
+++ Paulas_Rengoering/src/views/GuideView.vue
@@ -5,42 +5,17 @@
         <img class="object-cover" src="../assets/cleaningimg2.jpg" alt="Cleaning lady">
         
         <div class="absolute inset-0 flex flex-col items-center justify-center">
-            <h1 class="text-white text-3xl md:text-4xl lg:text-5xl pb-8">PRISER HOS PAULAS RENGØRING</h1>
-
-            <div class="flex flex-col space-y-4 px-24 text-sm pt-5 md:flex-row md:justify-center md:space-x-4 lg:flex-row lg:space-x-4 lg:space-y-0 lg:justify-center lg:text-sm lg:pt-5">
-                <button class="bg-white dark:bg-white py-3 px-8 text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
-                    Jeg tilbyder
-                </button>
-                <button class="bg-transparent hover:bg-blue-500 dark:bg-white py-3 px-8 text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
-                    Kontakt mig
-                </button>
-            </div>
+            <h1 class="text-white text-xs md:text-xl lg:text-xl pb-2 pt-20 md:pb-4 lg:pb-6 lg:pt-0">GUIDE</h1>
+            <p class="text-white text-base md:text-4xl lg:text-5xl pb-0 md:pb-2 lg:pb-2">SÅDAN FÅR DU MEST UD AF DIN</p>
+            <p class="text-white text-base md:text-4xl lg:text-5xl pb-8 md:pb-4 lg:pb-0">RENGØRINGSHJÆLP</p>
         </div>
     </div>
-
-
-
-    <!-- <div class="">
-        <div class="h-full w-full relative">
-            <img src="../assets/cleaningimg2.jpg" alt=""> 
-
-            <div class="absolute z-10 pt-40 md:pt-80 lg:pt-40">
-                <div>
-                    <p class="text-white dark:text-white text-1xl md:text-xl lg:text-md lg:tracking-wider">PROFESSIONEL RENGØRING</p>
-                    <h2 class="text-white dark:text-white pb-5 text-3xl md:text-4xl lg:text-5xl py-1">MED DIG SOM KUNDE I CENTRUM</h2>
-                </div>
-                <div class="flex flex-col space-y-4 px-24 text-sm pt-5 md:flex-row md:justify-center md:space-x-4 lg:flex-row lg:space-x-4 lg:space-y-0 lg:justify-center lg:text-sm lg:pt-5">
-                    <button class="bg-white dark:bg-white py-3 px-8 hover:beige text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
-                            JEG TILBYDER
-                    </button>
-                    <button class="bg-transparent py-3 px-8 dark:bg-transparent hover:bg-transparent border-2 text-white dark:text-white font-semibold lg:py-2 lg:px-8 rounded-full">
-                            KONTAKT MIG
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div> -->
 </section>
+
+<!-- Main section -->
+<main>
+
+</main>
 </template>
   
 <script setup>
