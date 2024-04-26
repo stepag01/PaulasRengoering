647910089a8449712c1ab2af9c3ade4f3dd1fe2a
stepag01 <stepag01@easv365.dk>
2024-04-26 15:36:52 +0200

    Book et formøde-sektion

--- Paulas_Rengoering/src/views/GuideView.vue
+++ Paulas_Rengoering/src/views/GuideView.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- Hero section -->
 <section>
-    <div class="relative h-full w-full">
+    <div class="relative h-full w-full md:h-full md:w-full lg:h-full lg:w-full">
         <img class="object-cover" src="../assets/cleaningimg2.jpg" alt="Cleaning lady">
         
         <div class="absolute inset-0 flex flex-col items-center justify-center">
@@ -14,14 +14,31 @@
 
 <!-- Main section -->
 <main>
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-align:center text-base font-semibold md:text-xl md:font-semibold lg:text-2xl lg:font-semibold pt-4 pb-2 md:pt-6 md:pb-4 lg:pt-8 lg:pb-6">OVERVEJELSER INDEN VALG AF RENGØRINGSHJÆLP</h1>
-        <p class="text-align:center text-sm md:text-base lg:text-base">Du går med overvejelserne om, at få hjælp til rengøringen af dit hjem. Du ved, at det vil give dig den ro, og det overskud i hverdagen som du mangler. Du vil derfor gerne ansætte en kyndig person til denne opgave, men hvordan får du mest ud af din rengøringshjælp? Jeg har samlet nogle råd til dig i den indledende fase, som sikrer dig at du får mest muligt ud af din rengøringshjælp, og bliver mest mulig tilfreds. </p>
+    <div class="flex flex-col items-center justify-center h-72">
+        <h1 class="text-align:center text-base font-semibold md:text-xl md:font-semibold lg:text-2xl lg:font-semibold pt-4 pb-2 md:pt-6 md:pb-4 lg:pt-2 lg:pb-6">OVERVEJELSER INDEN VALG AF RENGØRINGSHJÆLP</h1>
+        <p class="lg:ml-28 lg:mr-28 text-sm md:text-base lg:text-base lg:pb-10">Du går med overvejelserne om, at få hjælp til rengøringen af dit hjem. Du ved, at det vil give dig den ro, og det overskud i hverdagen som du mangler. Du vil derfor gerne ansætte en kyndig person til denne opgave, men hvordan får du mest ud af din rengøringshjælp? Jeg har samlet nogle råd til dig i den indledende fase, som sikrer dig at du får mest muligt ud af din rengøringshjælp, og bliver mest mulig tilfreds. </p>
     </div>
 
 
 
+<!--Book et formøde-sektion-->
+    <div class="flex-col ml-12 lg:ml-28 h-1/3 w-full lg:flex lg:flex-row lg:h-1/4 lg:w-full">
+        <div class="w-3/4 lg:w-1/3">
+            <img class="lg:object-cover" src="../assets/Elderly-man.jpg" alt="Elderly man with tree behind him">
+        </div>
+        <div class="bg-lightgray w-3/4 lg:w-1/2 ">
+            <h1>BOOK ET FORMØDE</h1>
+            <p class="">Det er altid en god idé at booke et formøde, hvor du får lejlighed til at snakke med den person der skal stå for rengøringen af dit hjem. Det er dit hjem, og du skal føle dig tryg i samarbejdet. Rengøringshjælpen får mulighed for at se dit hjem, inden samarbejdet bliver sat i gang, og det vil give dem en god fornemmelse af, hvor meget tid de skal sætte af til de opgaver, du ønsker hjælp til. </p>
+        </div>
+    </div>  
     
+    
+<!--Udarbejd en arbejdsplan-sektion-->
+    <div>
+        <div>
+            <h1>Helloooo</h1>
+        </div>
+    </div>
 </main>
 </template>
   
