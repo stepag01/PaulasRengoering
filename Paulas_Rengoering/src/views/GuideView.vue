1507fc24e9fd37c8f896497415eb7ee964072aea
stepag01 <stepag01@easv365.dk>
2024-04-26 13:30:13 +0200

    Hero-image +  text and button overlay inserted

--- Paulas_Rengoering/src/views/GuideView.vue
+++ Paulas_Rengoering/src/views/GuideView.vue
@@ -1,20 +1,46 @@
 <template>
-    <main>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="" alt="Sunset in the mountains">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p class="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+<!-- Hero section -->
+<section>
+    <div class="relative h-full w-full">
+        <img class="object-cover" src="../assets/cleaningimg2.jpg" alt="Cleaning lady">
+        
+        <div class="absolute inset-0 flex flex-col items-center justify-center">
+            <h1 class="text-white text-3xl md:text-4xl lg:text-5xl pb-8">PRISER HOS PAULAS RENGØRING</h1>
+
+            <div class="flex flex-col space-y-4 px-24 text-sm pt-5 md:flex-row md:justify-center md:space-x-4 lg:flex-row lg:space-x-4 lg:space-y-0 lg:justify-center lg:text-sm lg:pt-5">
+                <button class="bg-white dark:bg-white py-3 px-8 text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
+                    Jeg tilbyder
+                </button>
+                <button class="bg-transparent hover:bg-blue-500 dark:bg-white py-3 px-8 text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
+                    Kontakt mig
+                </button>
             </div>
         </div>
-    </main>
+    </div>
+
+
+
+    <!-- <div class="">
+        <div class="h-full w-full relative">
+            <img src="../assets/cleaningimg2.jpg" alt=""> 
+
+            <div class="absolute z-10 pt-40 md:pt-80 lg:pt-40">
+                <div>
+                    <p class="text-white dark:text-white text-1xl md:text-xl lg:text-md lg:tracking-wider">PROFESSIONEL RENGØRING</p>
+                    <h2 class="text-white dark:text-white pb-5 text-3xl md:text-4xl lg:text-5xl py-1">MED DIG SOM KUNDE I CENTRUM</h2>
+                </div>
+                <div class="flex flex-col space-y-4 px-24 text-sm pt-5 md:flex-row md:justify-center md:space-x-4 lg:flex-row lg:space-x-4 lg:space-y-0 lg:justify-center lg:text-sm lg:pt-5">
+                    <button class="bg-white dark:bg-white py-3 px-8 hover:beige text-darkgray dark:text-darkgray font-semibold lg:py-2 lg:px-8 rounded-full">
+                            JEG TILBYDER
+                    </button>
+                    <button class="bg-transparent py-3 px-8 dark:bg-transparent hover:bg-transparent border-2 text-white dark:text-white font-semibold lg:py-2 lg:px-8 rounded-full">
+                            KONTAKT MIG
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</section>
 </template>
   
 <script setup>
