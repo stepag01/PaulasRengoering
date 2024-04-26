dfab7746a11a9cd66f56f297c60bb580fe30941e
stepag01 <stepag01@easv365.dk>
2024-04-26 14:46:22 +0200

    Section with h1 and p

--- Paulas_Rengoering/src/views/GuideView.vue
+++ Paulas_Rengoering/src/views/GuideView.vue
@@ -14,7 +14,14 @@
 
 <!-- Main section -->
 <main>
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-align:center text-base font-semibold md:text-xl md:font-semibold lg:text-2xl lg:font-semibold pt-4 pb-2 md:pt-6 md:pb-4 lg:pt-8 lg:pb-6">OVERVEJELSER INDEN VALG AF RENGØRINGSHJÆLP</h1>
+        <p class="text-align:center text-sm md:text-base lg:text-base">Du går med overvejelserne om, at få hjælp til rengøringen af dit hjem. Du ved, at det vil give dig den ro, og det overskud i hverdagen som du mangler. Du vil derfor gerne ansætte en kyndig person til denne opgave, men hvordan får du mest ud af din rengøringshjælp? Jeg har samlet nogle råd til dig i den indledende fase, som sikrer dig at du får mest muligt ud af din rengøringshjælp, og bliver mest mulig tilfreds. </p>
+    </div>
 
+
+
+    
 </main>
 </template>
   
