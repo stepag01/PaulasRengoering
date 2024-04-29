13065dd2682d2870c1ee7629da3ca2371b35ceff
stepag01 <stepag01@easv365.dk>
2024-04-29 17:39:41 +0200

    Changes made to Overvejelses-sektion and Book et møde-sektion

--- Paulas_Rengoering/src/views/GuideView.vue
+++ Paulas_Rengoering/src/views/GuideView.vue
@@ -12,11 +12,11 @@
     </div>
 </section>
 
-<!-- Main section -->
+<!-- Main section, OVERVEJELSES-SEKTION -->
 <main>
-    <div class="flex justify-center flex-col h-72 md:h-60">
-        <h1 class="ml-4 mr-4 pb-2 md:ml-8 md:mr-8 font-semibold text-sm md:text-lg md:pt-4 md:pb-4 lg:text-xl lg:ml-20 lg:mr-20">OVERVEJELSER INDEN VALG AF RENGØRINGSHJÆLP</h1>
-        <p class="text-justify ml-4 mr-4 md:ml-8 md:mr-8 lg:ml-28 lg:mr-28 text-sm md:text-base md:pb-10 lg:text-base lg:ml-20 lg:mr-20 lg:pb-10">Du går med overvejelserne om, at få hjælp til rengøringen af dit hjem. Du ved, at det vil give dig den ro, og det overskud i hverdagen som du mangler. Du vil derfor gerne ansætte en kyndig person til denne opgave, men hvordan får du mest ud af din rengøringshjælp? Jeg har samlet nogle råd til dig i den indledende fase, som sikrer dig at du får mest muligt ud af din rengøringshjælp, og bliver mest mulig tilfreds. </p>
+    <div class="flex flex-col justify-center text-justify h-72 md:h-60 lg:h-60">
+        <h1 class="ml-4 mr-4 pb-2 md:ml-8 md:mr-8 font-semibold text-sm md:text-lg md:pt-4 md:pb-4 lg:text-xl lg:ml-28 lg:mr-20">OVERVEJELSER INDEN VALG AF RENGØRINGSHJÆLP</h1>
+        <p class="ml-4 mr-4 md:ml-8 md:mr-8 lg:ml-28 lg:mr-28 text-sm md:text-base md:pb-10 lg:text-base lg:ml-20 lg:mr-20 lg:pb-10">Du går med overvejelserne om, at få hjælp til rengøringen af dit hjem. Du ved, at det vil give dig den ro, og det overskud i hverdagen som du mangler. Du vil derfor gerne ansætte en kyndig person til denne opgave, men hvordan får du mest ud af din rengøringshjælp? Jeg har samlet nogle råd til dig i den indledende fase, som sikrer dig at du får mest muligt ud af din rengøringshjælp, og bliver mest mulig tilfreds. </p>
     </div>
 
 
@@ -26,7 +26,7 @@
         <div class="w-3/4 lg:w-1/4">
             <img class="lg:object-cover" src="../assets/Elderly-man.jpg" alt="Elderly man with tree behind him">
         </div>
-        <div class="bg-lightgray w-3/4 lg:w-1/2">
+        <div class="bg-lightgray w-3/4 lg:w-1/2 p-4">
             <h1 class="font-semibold text-base pt-2 pb-2 md:text">BOOK ET FORMØDE</h1>
             <p class="">Det er altid en god idé at booke et formøde, hvor du får lejlighed til at snakke med den person der skal stå for rengøringen af dit hjem. Det er dit hjem, og du skal føle dig tryg i samarbejdet. Rengøringshjælpen får mulighed for at se dit hjem, inden samarbejdet bliver sat i gang, og det vil give dem en god fornemmelse af, hvor meget tid de skal sætte af til de opgaver, du ønsker hjælp til. </p>
         </div>
