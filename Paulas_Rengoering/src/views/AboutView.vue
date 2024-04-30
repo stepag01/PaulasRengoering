d78de175b7f042fb49b8890e9f2b7bc18235d615
stepag01 <stepag01@easv365.dk>
2024-04-30 13:14:47 +0200

    Hero section implemented on the About Page

--- Paulas_Rengoering/src/views/AboutView.vue
+++ Paulas_Rengoering/src/views/AboutView.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    This is the about page
-  </div>
+<!-- Hero section -->
+<section>
+    <div class="relative h-full w-full md:h-full md:w-full lg:h-full lg:w-full">
+        <img class="object-cover" src="../assets/cleaning-articles.jpg" alt="Cleaning articales on the floor">
+        
+        <div class="absolute inset-0 flex flex-col items-center justify-center">
+            <h1 class="text-white text-xs md:text-xl lg:text-xl pb-2 pt-20 md:pb-4 lg:pb-6 lg:pt-0">OM FIRMAET</h1>
+            <p class="text-white text-base md:text-4xl lg:text-5xl pb-0 md:pb-2">PAULAS RENGØRING</p>
+        </div>
+    </div>
+</section>
+
+
+
+
+
 </template>
 
 <script setup>
