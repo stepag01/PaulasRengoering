8b4277aff11f84d84e4006afb0da237f72936ede
stepag01 <stepag01@easv365.dk>
2024-04-30 15:06:04 +0200

    Value-section implemented

--- Paulas_Rengoering/src/views/AboutView.vue
+++ Paulas_Rengoering/src/views/AboutView.vue
@@ -12,7 +12,21 @@
 </section>
 
 
-
+<div class="flex flex-col justify justify-center w-3/4 mb-16 mx-auto h-1/2 lg:flex-row lg:pt-20 lg:w-full lg:mx-0 lg:mb-20">
+    <div class="lg:w-2/4 lg:pl-8 lg:pt-8 lg:pr-8 bg-lightgray">
+        <h1 class="font-semibold lg:text-base lg:pb-10">MINE VÆRDISÆT</h1>
+        <h3 class="font-semibold lg:text-xl lg:pb-2">TILLID</h3>
+        <p class="lg:text-base lg:pb-10">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+        <h3 class="font-semibold lg:text-xl lg:pb-2">KUNDETILFREDSHED</h3>
+        <p class="lg:text-base lg:pb-10">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+        <h3 class="font-semibold text-base pb-2 lg:text-xl lg:pb-2">TÆT SAMARBEJDE</h3>
+        <p class="text-sm md:text-base lg:text-base">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+      </div>
+      
+    <div class="lg:w-1/3">
+            <img class="" src="../assets/value.jpg" alt="Woman sitting at desk with computer"/>
+    </div>
+</div>
 
 
 </template>
