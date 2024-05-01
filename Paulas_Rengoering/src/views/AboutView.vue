c1a23574b13878ec09bbbdef58286d30baa6b887
stepag01 <stepag01@easv365.dk>
2024-05-01 11:49:23 +0200

    PaulasRengøring-Erhverv-section small and medium screen size responsiveness

--- Paulas_Rengoering/src/views/AboutView.vue
+++ Paulas_Rengoering/src/views/AboutView.vue
@@ -31,10 +31,24 @@
 
 
 <!--PaulasRengøring-paragraf-sektion-->
-<div class="flex justify justify-center bg-lightgray h-52 max-h-32 min-h-60 w-full mt-16 mb-20 pt-6 px-8 md:h-32 md:max-h-40 md:pt-16 md:mb-16 lg:h-60 lg:w-full lg:pt-10 lg:mb-20">
+<div class="flex justify justify-center h-52 max-h-32 min-h-60 w-full mt-16 mb-0 pt-6 px-8 md:h-32 md:max-h-40 md:pt-16 md:mb-0 lg:h-60 lg:w-full lg:pt-10 lg:mb-20">
     <div class="lg:w-2/3 lg:text-center">
-        <h1 class="font-semibold text-lg md:text-2xl lg:text-3xl lg:pb-4">PAULAS RENGØRING</h1>
+        <h1 class="font-semibold pt-4 text-lg md:text-2xl lg:text-3xl lg:pb-4">PAULAS RENGØRING</h1>
         <p class="text-sm md:text-base lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis culpa, dolore quidem libero obcaecati aliquam neque sit error dignissimos doloremque tempora asperiores, in fugit, consequuntur nulla? Quisquam, neque laborum.</p>
+    </div>
+</div>
+
+<!--PaulasRengøring-Erhverv-sektion-->
+<div class="flex flex-col justify justify-center h-1/2 w-full p-8 md:flex-row md:w-full">
+    <div class="md:w-1/2">
+        <img src="../assets/Elderly-man.jpg" alt="">
+    </div>
+
+    <div class="bg-lightgray p-4 md:w-1/2 md:pl-4 md:pt-2">
+        <h1 class="text-xs font-semibold pb-2 md:pb-0">OM FIRMAET</h1>
+        <h3 class="text-lg font-semibold pb-4 md:text-xl">PAULAS RENGØRING</h3>
+        <p class="text-sm font-semibold pb-1">OVER 10 ÅRS ERFARING</p>
+        <p class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis deserunt cupiditate accusamus quasi ipsum. Illo velit consectetur aliquid consequuntur numquam, accusamus harum, vero mollitia dolorum tenetur, molestias rerum consequatur ab.</p>
     </div>
 </div>
 
